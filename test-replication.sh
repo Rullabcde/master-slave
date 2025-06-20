29a12ffceb778d8cdeb5bb73f492e826ffb73be4
Rullabcde <choirulrasyid09@gmail.com>
2025-06-20 13:59:58 +0700

    update test replica and create database

--- test-replication.sh
+++ test-replication.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# Test 1: Insert data di Master
-echo "1. Insert data baru di Master..."
-echo "1. Insert data baru di Master..."
-docker exec mysql-master mysql -uroot -psiswa -e "USE testdb; INSERT INTO employees (name, email, department, salary) VALUES ('Test User', 'user@company.com', 'ID', 55000.00);"
+
+# Cek data di Slave 1
+echo "Cek data di Slave 1 sebelum insert..."
+SLAVE1_DATA=$(docker exec -i mysql-slave1 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;")
+echo "Data di Slave 1:" $SLAVE1_DATA
+
+# Cek data di Slave 2
+echo "Cek data di Slave 2 sebelum insert..."
+SLAVE2_DATA=$(docker exec -i mysql-slave2 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;")
+echo "Data di Slave 2:" $SLAVE2_DATA
+
+
+# Insert data di Master
+echo "Insert data baru di Master..."
+docker exec -i mysql-master mysql -uroot -psiswa -e "USE testdb; INSERT INTO employees (name, email, department, salary) VALUES ('Test User', 'user@company.com', 'ID', 55000.00);"
 
 sleep 2
 
-# Test 2: Cek data di Slave 1
-echo "2. Cek data di Slave 1..."
-SLAVE1_COUNT=$(docker exec mysql-slave1 mysql -uroot -psiswa -se "SELECT COUNT(*) FROM testdb.employees;")
-echo "Total records di Slave 1: $SLAVE1_COUNT"
+# Cek data di Slave 1
+echo "Cek data di Slave 1 setelah insert..."
+SLAVE1_DATA=$(docker exec -i mysql-slave1 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;")
+echo "Data di Slave 1:" $SLAVE1_DATA
 
-# Test 3: Cek data di Slave 2
-echo "3. Cek data di Slave 2..."
-SLAVE2_COUNT=$(docker exec mysql-slave2 mysql -uroot -psiswa -se "SELECT COUNT(*) FROM testdb.employees;")
-echo "Total records di Slave 2: $SLAVE2_COUNT"
-
+# Cek data di Slave 2
+echo "Cek data di Slave 2 setelah insert..."
+SLAVE2_DATA=$(docker exec -i mysql-slave2 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;")
+echo "Data di Slave 2:" $SLAVE2_DATA
