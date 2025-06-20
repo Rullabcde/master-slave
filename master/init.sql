15c1978dca227cc53107c97a8bc65f0dfbe13cb0
Rullabcde <choirulrasyid09@gmail.com>
2025-06-20 13:26:56 +0700

    update master/init.sql

--- master/init.sql
+++ master/init.sql
@@ -1,4 +1,4 @@
-CREATE USER 'slave1'@'%' mysql_native_password BY 'siswa';
+CREATE USER 'slave1'@'%' IDENTIFIED WITH mysql_native_password BY 'siswa';
 GRANT REPLICATION SLAVE ON *.* TO 'slave1'@'%';
 FLUSH PRIVILEGES;
 
