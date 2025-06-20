75cc4269c884da0ba3a89d235a7358ced63b4bad
Rullabcde <choirulrasyid09@gmail.com>
2025-06-20 14:29:59 +0700

    update script final

--- setup-replication.sh
+++ setup-replication.sh
@@ -27,6 +27,12 @@ CHANGE MASTER TO
     MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;
 "
+# Create user for read access
+docker exec -i mysql-slave1 mysql -uroot -psiswa -e "
+CREATE USER 'rullabcd'@'%' IDENTIFIED BY 'rullabcd';
+GRANT SELECT ON *.* TO 'rullabcd'@'%';
+FLUSH PRIVILEGES;
+"
 
 # Setup Slave 2
 echo "Konfigurasi Slave 2"
@@ -39,6 +45,12 @@ CHANGE MASTER TO
     MASTER_LOG_FILE='$MASTER_LOG_FILE',
     MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;
+"
+# Create user for read access
+docker exec -i mysql-slave1 mysql -uroot -psiswa -e "
+CREATE USER 'rullabcd'@'%' IDENTIFIED BY 'rullabcd';
+GRANT SELECT ON *.* TO 'rullabcd'@'%';
+FLUSH PRIVILEGES;
 "
 
 # Cek status replikasi
--- test-replication.sh
+++ test-replication.sh
@@ -3,11 +3,11 @@
 
 # Cek data di Slave 1
 echo "Cek data di Slave 1 sebelum insert..."
-docker exec -i mysql-slave1 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;"
+docker exec -i mysql-slave1 mysql -urullabcd -prullabcd -e "SELECT * FROM testdb.employees;"
 
 # Cek data di Slave 2
 echo "Cek data di Slave 2 sebelum insert..."
-docker exec -i mysql-slave2 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;"
+docker exec -i mysql-slave2 mysql -urullabcd -prullabcd -e "SELECT * FROM testdb.employees;"
 
 # Insert data di Master
 echo "Insert data baru di Master..."
@@ -17,8 +17,18 @@ sleep 2
 
 # Cek data di Slave 1
 echo "Cek data di Slave 1 setelah insert..."
-docker exec -i mysql-slave1 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;"
+docker exec -i mysql-slave1 mysql -urullabcd -prullabcd -e "SELECT * FROM testdb.employees;"
 
 # Cek data di Slave 2
 echo "Cek data di Slave 2 setelah insert..."
-docker exec -i mysql-slave2 mysql -uroot -psiswa -e "SELECT * FROM testdb.employees;"
+docker exec -i mysql-slave2 mysql -urullabcd -prullabcd -e "SELECT * FROM testdb.employees;"
+
+# Coba delete data di Slave 1
+echo "Coba delete data di Slave 1..."
+docker exec -i mysql-slave1 mysql -urullabcd -prullabcd -e "USE testdb; DELETE FROM employees WHERE name='Test User';"
+echo "Pasti gagal karena Slave tidak bisa melakukan operasi write."
+
+# Coba delete data di Slave 2
+echo "Coba delete data di Slave 2..."
+docker exec -i mysql-slave2 mysql -urullabcd -prullabcd -e "USE testdb; DELETE FROM employees WHERE name='Test User';"
+echo "Pasti gagal karena Slave tidak bisa melakukan operasi write."
